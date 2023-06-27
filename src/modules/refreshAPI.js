f7168227ef41b3a613c25180028c1decababa7d8
AsumVictor <iamasum369@gmail.com>
2023-06-27 17:42:08 +0000

    Set API URL and result function

--- src/modules/refreshAPI.js
+++ src/modules/refreshAPI.js
@@ -1,8 +1,10 @@
 import { API_URL } from "./Variables";
 const refresh = async () => {
   try {
-    const result = await fetch(API_URL);
-    console.log(result);
+    const res = await fetch(API_URL);
+    const data = await res.json()
+    const scores = data.result
+    console.log(scores);
   } catch (error) {
     console.log(error);
   }
