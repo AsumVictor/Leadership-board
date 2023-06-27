6c9ca1606106e308eb8a3c09f5bf8c0e922f3609
AsumVictor <iamasum369@gmail.com>
2023-06-27 11:04:21 +0000

    Setup webpack

---
+++ webpack.common.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      }
+    ],
+  },
+};
